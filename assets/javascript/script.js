24ce1293cb4de7db04127f9f841e7cb3e7da8df9
giobocca03 <gio.bocca03@gmail.com>
2024-12-17 14:20:48 +0100

    rivista disposizione pulsanti e modificato css

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,14 +9,26 @@ const counterDiv = document.createElement("div");
 counterDiv.className = "counter-container";
 container.appendChild(counterDiv);
 
-const buttonsDiv = document.createElement("div");
-buttonsDiv.className = "buttons-container";
-container.appendChild(buttonsDiv);
-
 const buttonMeno = document.createElement("button");
 buttonMeno.className = "buttons";
-buttonsDiv.appendChild(buttonMeno);
+counterDiv.appendChild(buttonMeno);
+buttonMeno.innerHTML = "-";
+
+const counter = document.createElement("p");
+counter.className = "counter";
+counterDiv.appendChild(counter);
+counter.innerHTML = "0";
 
 const buttonPiu = document.createElement("button");
 buttonPiu.className = "buttons";
-buttonsDiv.appendChild(buttonPiu);
+counterDiv.appendChild(buttonPiu);
+buttonPiu.innerHTML = "+";
+
+const resetDiv = document.createElement("div");
+resetDiv.className = "reset-container";
+container.appendChild(resetDiv);
+
+const buttonReset = document.createElement("button");
+buttonReset.className = "button-reset";
+resetDiv.appendChild(buttonReset);
+buttonReset.innerHTML = "-";
