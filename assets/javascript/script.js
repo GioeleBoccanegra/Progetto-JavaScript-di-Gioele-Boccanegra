9401b11a922722300447315a6338a8f9f524f8d5
giobocca03 <gio.bocca03@gmail.com>
2024-12-17 14:53:06 +0100

    rivisto ancora codice

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -7,7 +7,6 @@ container.appendChild(counterDiv);
 
 const buttonMeno = document.createElement("button");
 buttonMeno.className = "buttons";
-buttonMeno.setAttribute("data-action", "decrease");
 buttonMeno.innerHTML = "-";
 counterDiv.appendChild(buttonMeno);
 
@@ -18,7 +17,6 @@ counterDiv.appendChild(counter);
 
 const buttonPiu = document.createElement("button");
 buttonPiu.className = "buttons";
-buttonPiu.setAttribute("data-action", "increase");
 buttonPiu.innerHTML = "+";
 counterDiv.appendChild(buttonPiu);
 
@@ -28,30 +26,21 @@ container.appendChild(resetDiv);
 
 const buttonReset = document.createElement("button");
 buttonReset.className = "button-reset";
-buttonReset.setAttribute("data-action", "reset");
 buttonReset.innerHTML = "Reset";
 resetDiv.appendChild(buttonReset);
 
-function updateCounter(action) {
-  const currentValue = parseInt(counter.textContent);
-  switch (action) {
-    case "increase":
-      counter.textContent = currentValue + 1;
-      break;
-    case "decrease":
-      counter.textContent = currentValue - 1;
-      break;
-    case "reset":
-      counter.textContent = 0;
-      break;
-    default:
-      break;
-  }
+function aumenta() {
+  counter.textContent = parseInt(counter.textContent) + 1;
 }
 
-container.addEventListener("click", function (event) {
-  if (event.target.tagName === "BUTTON") {
-    const action = event.target.getAttribute("data-action");
-    updateCounter(action);
-  }
-});
+function diminuisci() {
+  counter.textContent = parseInt(counter.textContent) - 1;
+}
+
+function resetta() {
+  counter.textContent = 0;
+}
+
+buttonPiu.addEventListener("click", aumenta);
+buttonMeno.addEventListener("click", diminuisci);
+buttonReset.addEventListener("click", resetta);
